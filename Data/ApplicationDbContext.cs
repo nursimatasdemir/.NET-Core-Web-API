64a2ca2a861af60b9aa509f9579b67a8ee29cd59
nursimatasdemir <nrsmtsdmr@gmail.com>
2024-09-13 15:08:26 +0300

    Created database and added migrations to update and create it

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -1,7 +1,21 @@
+using api.Models;
+using Microsoft.EntityFrameworkCore;
+//using Npgsql.EntityFrameworkCore.PostgreSQL;
+
 namespace api.Data;
 
-//this class is for searching individu
-public class ApplicationDbContext
-{
-    
-}
+//this class is for searching individual tables and to transform database tables to objects
+//AppDBContext is also an object
+    public class ApplicationDbContext : DbContext
+    {
+        //base constructor is going to add the info we gave to the database
+        public ApplicationDbContext(DbContextOptions dbContextOptions) : base(dbContextOptions)
+        {
+            
+        }
+        //dbSet stands for a set of data that comes from database 'and' you can specify which form it should return
+        //deferred execution // data manipulation
+        public DbSet<Stock> Stock { get; set; }
+        public DbSet<Comment> Comment { get; set; }
+
+    }
--- Program.cs
+++ Program.cs
@@ -1,9 +1,19 @@
+using api.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+//created a service to builder it will connect to our database
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+{
+    options.UseNpgsql(connectionString:"Host=localhost;Database=WebAPIDb;Username=admin;Password=admin");  
+});
 
 var app = builder.Build();
 
